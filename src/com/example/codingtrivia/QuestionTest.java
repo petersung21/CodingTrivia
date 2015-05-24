09284845b9c13e6916b108d86b8e65378702ec43
Peter Sung <petersung21@gmail.com>
2015-05-24 13:05:35 -0400

    Peter Commit

--- src/com/example/codingtrivia/QuestionTest.java
+++ src/com/example/codingtrivia/QuestionTest.java
@@ -4,10 +4,31 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.json.JSONObject;
+
+import android.widget.Toast;
+import com.androidquery.*;
+import com.androidquery.callback.*;
+
 public class QuestionTest {
 	private static List<questions> allquestions = new ArrayList<questions>();
+	private static AQuery aq;
 	
 	public static void fillQuestions(){
+        String url = "http://nodejs-devsung.rhcloud.com/mysql";
+        
+        aq.ajax(url, JSONObject.class, new AjaxCallback<JSONObject>() {
+            @Override
+            public void callback(String url, JSONObject json, AjaxStatus status) {                    
+                if(json != null){                              
+                    //successful ajax call, show status code and json content
+                    Toast.makeText(aq.getContext(), status.getCode() + ":" + json.toString(), Toast.LENGTH_LONG).show();                
+                }else{                  
+                    //ajax error, show error code
+                    Toast.makeText(aq.getContext(), "Error:" + status.getCode(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 		if (allquestions.size()<=3){
 			allquestions.add(new questions(0,"What is not a property of static classes", "Static classes can not be instantiated",
 					"You can access static methods without instantiating class","You can't override static methods", "You can have a lone static class",
@@ -20,6 +41,7 @@ public class QuestionTest {
 					"Base class can not be instantiated only inherited from"));
 		}
 	}
+	
 	public static questions mixQuestions(){
 		Collections.shuffle(allquestions);
 		return allquestions.get(0);
